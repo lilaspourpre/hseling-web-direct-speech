3b3953cac3d92eb9f76cd8daa12913c4dd098f68
Sergey Sobko <ssobko@hse.ru>
2018-12-03 15:18:07 +0300

    WIP: Web Template for API

--- src/web/static/main.js
+++ src/web/static/main.js
@@ -1,3 +1,31 @@
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
 $(function () {
-  alert("Hello, world!");
+  var vars = getUrlVars(),
+      task_id = vars["task_id"];
+
+  if(task_id) {
+    var get_status = function () {
+      $.get("/web/status?task_id=" + task_id, function(data) {
+        if (data.ready) {
+          $(".lead").text(data.result);
+        } else {
+          setTimeout(get_status, 2000);
+        }
+      });
+    };
+
+    get_status();
+  }
 });
--- src/web/views.py
+++ src/web/views.py
@@ -1,6 +1,60 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+from django import forms
 
+import requests
+
+
+HSE_API_ROOT = "http://hse-api-web/"
 
 # Create your views here.
 def web_index(request):
-    return render(request, 'index.html', context={})
+    return render(request, 'index.html',
+                  context={})
+
+
+def web_main(request):
+    # content = requests.get(HSE_API_ROOT)
+    return render(request, 'main.html',
+                  context={})
+
+
+def web_status(request):
+    task_id = request.GET.get('task_id')
+    if task_id:
+        url = HSE_API_ROOT + "status/" + task_id
+        content = requests.get(url)
+        return JsonResponse(content.json())
+    return JsonResponse({"error": "No task id"})
+
+
+def handle_uploaded_file(f):
+    files = {'file': f}
+    url = HSE_API_ROOT + "upload"
+    content = requests.post(url, files=files)
+    file_id = content.json().get("file_id")
+
+    if file_id:
+        file_id = file_id[7:]
+        url = HSE_API_ROOT + "process/" + file_id
+        content = requests.get(url)
+
+    else:
+        raise Exception(content.json())
+
+    return content.json().get('task_id')
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
+
+
+def web_upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            task_id = handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('main?task_id=' + task_id)
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
